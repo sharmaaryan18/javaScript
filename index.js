da738ee41c0c0845adbe3d494f1bfaac4985d275
sharmaaryan18 <aryansharm3876@gmail.com>
2023-08-10 23:59:50 +0530

    java_basics

--- index.js
+++ index.js
@@ -27,7 +27,7 @@
 //     }
 // };
 
-//FACTORY FUNCTION- creates object and then return the object
+//FACTORY FUNCTION- creates object and then return the object 
 
 // function createRectangle() {
 //     return rectangle = {
@@ -55,9 +55,9 @@
 //         length:len,
 //         breadth:bre,
     
-//         // draw: function() {
-//         //     console.log('drawing rectangle');
-//         // }
+//         draw: function() {
+//             console.log('drawing rectangle');
+//         }
 //         //OR
 
 //         draw() {//method
@@ -170,24 +170,30 @@
 // for(let key in src) {
 //     dest[key]=src[key];
 // }
-// console.log(dest);
+
 
 // src.a++;
 // console.log(dest);
 
 //Object #2Assign
-let src = {   
-    a:10,
-    b:20,
-    c:30
- };
 
-let dest = Object.assign({},src);
-console.log(dest);
-src.a++;
-console.log(dest);
+
+// let src = {   
+//     a:10,
+//     b:20,
+//     c:30
+//  };
+
+//  let src2 = {value: 25};
+
+// let dest = Object.assign({},src,src2); we can also do coloningof  multiple  objects in one objects using assignfunction
+// console.log(dest);
+// src.a++;
+// console.log(dest);
 
 // //Oject colong#3
+
+
 // let src= {    
 //          a:10,
 //          b:20,
@@ -198,5 +204,73 @@ console.log(dest);
 //      console.log(dest);
 
 //      src.a++;
-//      console.log(dest);
+//      console.log(dest);//There will be no change in dest beacuse of src.a++ 
+
+//String-primitive
+
+// console.log("hello jee");
+
+// let lastName = '  Sharma';//string
+
+// let firstName = new String('Aryan');//Object
+
+//ON CONSOLE
+// 
+// lastName.length
+// 6
+// lastName[0];
+// 'S'
+// lastName.includes('Sh')
+// true
+// lastName.starts
+// undefined
+// lastName.startsWith('Sha')
+// true
+// lastName.endsWith('rma')
+// true
+// lastName.toUpperCase();
+// 'SHARMA'
+// lastName.toLocaleLowerCase();
+// 'sharma'
+// lastName.replace('Sh','K');
+// '  Karma'
+
+
+//TEMPLATE LITERAL
+//to movr in next line we use \n
+//to print single quote we use  \'
+
+// let message ='This is \n my \' "first" \n message';
+
+// console.log(message);
+
+// //so to OVERCOME THIS TEMPALTE LITERAL IS USED I.E ``
+
+// let lastName = 'Sharma';//string
+// let message1 =`
+// hii ${lastName}, 
+// This is my
+// first  message
+// Regards,
+// AryanSharma`;
+// console.log(message1);
+
+
+
+
+//CREATION OF DATE
+
+let date = new Date();
+console.log(date);
+
+let date2 = new Date('August 18 2003 7:00');
+console.log(date2);
+
+
+let date3 = new Date(2003, 7,18,5);
+console.log(date3);
+
+date3.setFullYear(2002);
+console.log(date3);
+
 
